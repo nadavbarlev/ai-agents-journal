7956b87c7871d34ec39b833d79957b2b7f93f891
nadavbarlev <nadav.bar.lev@gmail.com>
2025-10-11 01:11:38 +0300

    feat: day-04

---
+++ day-04/main.py
@@ -0,0 +1,44 @@
+import asyncio
+
+from agents import Agent, Runner, SQLiteSession
+from config import load_env
+
+
+async def main():
+    load_env()
+
+    # Chat Without Session
+    # agent = Agent(
+    #     name="Assistant",
+    #     instructions="Reply very concisely.",
+    # )
+    # message = input("> ")
+    # result = await Runner.run(agent, message)
+    # print(result.final_output)
+
+    # while True:
+    #     message_history = result.to_input_list()
+    #     print(f"Messages: {message_history}")
+
+    #     message = input("> ")
+    #     result = await Runner.run(
+    #         agent, message_history + [{"role": "user", "content": message}]
+    #     )
+    #     print(result.final_output)
+
+    # Chat With Session
+    agent = Agent(
+        name="Assistant",
+        instructions="Reply very concisely.",
+    )
+
+    session = SQLiteSession("conversation_day04")
+
+    while True:
+        user_message = input("> ")
+        response = await Runner.run(agent, user_message, session=session)
+        print(response.final_output)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
