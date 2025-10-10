5f8b78f59d86c067734ba170928caf77e6b1a0c0
nadavbarlev <nadav.bar.lev@gmail.com>
2025-10-10 23:05:12 +0300

    feat: day-02

---
+++ day-02/main.py
@@ -0,0 +1,30 @@
+import asyncio
+
+from agents import Agent, Runner
+from agents.extensions.models.litellm_model import LitellmModel
+from config import load_env
+
+
+async def main():
+    load_env()
+
+    # OpenAI Model
+    # agent = Agent(
+    #     name="Assistant",
+    #     model="gpt-4.1-nano",
+    #     instructions="You only respond in haikus.",
+    # )
+
+    # Non-OpenAI Model
+    agent = Agent(
+        name="Assistant",
+        model=LitellmModel(model="ollama/llama3.2:3b"),
+        instructions="You only respond in haikus.",
+    )
+
+    result = await Runner.run(agent, "Tell me about recursion in programming.")
+    print(result.final_output)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
