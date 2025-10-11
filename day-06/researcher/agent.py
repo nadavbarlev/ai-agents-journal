d293c2b5ddf4fd5885f8649d7dfcfd03ec9e3f7a
nadavbarlev <nadav.bar.lev@gmail.com>
2025-10-11 16:43:17 +0300

    feat: day-06

---
+++ day-06/researcher/agent.py
@@ -0,0 +1,45 @@
+import asyncio
+from pathlib import Path
+
+from jinja2 import Environment, FileSystemLoader
+from pydantic import BaseModel, Field
+
+from agents import Agent, Runner
+from agents.extensions.models.litellm_model import LitellmModel
+from config import load_env
+
+
+class BlogPostIdea(BaseModel):
+    title: str = Field(..., title="Title", description="The title of the blog post")
+    main_concepts: list[str] = Field(
+        ..., title="Main Concepts", description="Main concepts for the post"
+    )
+
+
+async def main(general_topic: str):
+    load_env()
+
+    # Load instructions from file
+    script_dir = Path(__file__).parent
+    env = Environment(loader=FileSystemLoader(script_dir))
+    template = env.get_template("instructions.md")
+
+    # Researcher Agent
+    researcher_agent = Agent(
+        name="MarketResearcher",
+        model=LitellmModel(model="openrouter/anthropic/claude-3-5-sonnet"),
+        # Removed output_type since claude doesn't support structured
+        # json outputs via OpenRouter
+        # output_type=list[BlogPostIdea],
+        instructions=template.render(general_topic=general_topic),
+    )
+
+    result = await Runner.run(
+        researcher_agent,
+        "Create 5 blog posts subject lines and main concept",
+    )
+    print(result.raw_responses)
+
+
+if __name__ == "__main__":
+    asyncio.run(main("weather"))
---
+++ day-06/researcher/instructions.md
@@ -0,0 +1,6 @@
+You are a market researcher and your job is to suggest cool ideas
+for blog posts about {{ general_topic }}. I will send you ideas and you will help me turn
+them into engaging posts, Or I will send you topics and you will help
+to focus me on the best viral ideas in the {{ general_topic }} niche.
+
+Focus specifically on creating blog post ideas that would be engaging and viral within the {{ general_topic }} community and audience.
